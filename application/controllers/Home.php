4539f79d3ffb14d8c8c4a5544c2d74a25f2966f5
Kevin Risqi <kevinrisqi22@gmail.com>
2019-03-15 00:16:37 +0700

    Tahap slicing tampilan back end

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,12 @@ Class Home extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('home/index');
+        $this->load->view('admin/templates/head');
+        $this->load->view('admin/templates/navbar');
+        $this->load->view('admin/templates/leftbar');
+        $this->load->view('admin/templates/contentheader');
+        $this->load->view('admin/home/index');
+        $this->load->view('admin/templates/footer');
     }
 
 }
---
+++ application/views/admin/home/index.php
@@ -0,0 +1,104 @@
+
+
+  <!-- Main content -->
+  <section class="content">
+      <!-- Info boxes -->
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">CPU Traffic</span>
+              <span class="info-box-number">90<small>%</small></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Likes</span>
+              <span class="info-box-number">41,410</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix visible-sm-block"></div>
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Sales</span>
+              <span class="info-box-number">760</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Members</span>
+              <span class="info-box-number">2,000</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+  
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box">
+            <div class="box-header with-border">
+
+              <div class="box-tools pull-right">
+              
+              </div>
+            </div>
+            <!-- /.box-header -->
+            
+            <!-- ./box-body -->
+            
+            <!-- /.box-body -->
+          
+          <!-- /.box -->
+          
+            <!-- /.col -->
+
+            
+          <!-- /.row -->
+
+          <!-- TABLE: LATEST ORDERS -->
+          
+            <!-- /.box-footer -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  
