7647d5ff3d65053ffea10ca525f07699cfc4264d
Kevin Risqi <kevinrisqi22@gmail.com>
2019-03-18 01:03:22 +0700

    Perbaikan Template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,12 +4,8 @@ Class Home extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('admin/templates/head');
-        $this->load->view('admin/templates/navbar');
-        $this->load->view('admin/templates/leftbar');
-        $this->load->view('admin/templates/contentheader');
-        $this->load->view('admin/home/index');
-        $this->load->view('admin/templates/footer');
+        $this->load->view('admin/templates/index');
+        
     }
 
 }
