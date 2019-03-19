724808e5d8517949785890283b5c5ef98b9b6131
Kevin Risqi <kevinrisqi22@gmail.com>
2019-03-19 18:50:20 +0700

    Penambahan dan passing data pada controller home

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -4,7 +4,8 @@ Class Home extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('admin/templates/index');
+        $data['content'] = 'admin/dashboard'; 
+        $this->load->view('admin/templates/index',$data);
         
     }
 
