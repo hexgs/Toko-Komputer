aecf369ba031ff538da9e7ef331cce52818ccdca
yogaibnu <yogaibnudinataa@gmail.com>
2019-03-21 19:35:00 +0700

    add function __construct pada controller User

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -3,10 +3,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('User_model');
+    }
+
     public function index()
     {
         $data['judul'] = 'User - Page';
-
         $data['user'] = $this->User_model->getAllUser();
         $data['content'] = 'admin/user';
         $this->load->view('admin/templates/index',$data);
