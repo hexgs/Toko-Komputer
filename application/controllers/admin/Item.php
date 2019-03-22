0ad08f9edf5f2f0b0ca9266f7b08ede63cf7bcf7
irhamrizaldy <rirham12@gmail.com>
2019-03-22 09:17:38 +0700

    Menambahkan button 'tambah item'

--- application/controllers/admin/Item.php
+++ application/controllers/admin/Item.php
@@ -1,17 +1,17 @@
-<?php   
+<?php 
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Item extends CI_Controller {
+class Item extends CI_Controller
+{
 
-    
+
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Item_model');
-        
     }
-    
+
 
     public function index()
     {
@@ -19,12 +19,12 @@ class Item extends CI_Controller {
         $data['title'] = 'Item - Page';
         $data['judul'] = 'Item';
         $data['content'] = 'admin/item';
-        $this->load->view('admin/templates/index',$data);
-        
+        $this->load->view('admin/templates/index', $data);
     }
 
+    public function tambah()
+    { }
 }
 
 /* End of file Item.php */
-
-?>
+ 
--- application/views/admin/item.php
+++ application/views/admin/item.php
@@ -1,42 +1,45 @@
 <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
+    <div class="col-xs-12">
+        <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Item List</h3>
+                <h3 class="box-title">Item List</h3>
+            </div>
+            <div class="container">
+                <a href="<?php base_url('') ?>admin/item/tambah" class="btn btn-primary">Tambah Item</a>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>No</th>
-                  <th>Nama</th>
-                  <th>Harga</th>
-                  <th>Stok</th>
-                  <th>Kategori</th>
-                  <th>Aksi</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php $nomor = 1; ?>
-                <?php foreach($item as $produk): ?>
-                <tr>
-                  <td><?php echo $nomor ?></td>
-                  <td><?php echo $produk['nama_produk'] ?></td>
-                  <td><?php echo $produk['harga'] ?></td>
-                  <td><?php echo $produk['stok'] ?></td>
-                  <td><?php echo $produk['id_kategori'] ?></td>
-                  <td>
-                      <a href="#" class="btn btn-primary">Detail</a>
-                      <a href="#" class="btn btn-warning">Ubah</a>
-                      <a href="#" class="btn btn-danger">Hapus</a>
-                  </td>
-                </tr>
-                <?php $nomor++; ?>
-                <?php endforeach; ?>
-                </tbody>
-              </table>
+                <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Harga</th>
+                            <th>Stok</th>
+                            <th>Kategori</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $nomor = 1; ?>
+                        <?php foreach ($item as $produk) : ?>
+                        <tr>
+                            <td><?php echo $nomor ?></td>
+                            <td><?php echo $produk['nama_produk'] ?></td>
+                            <td><?php echo $produk['harga'] ?></td>
+                            <td><?php echo $produk['stok'] ?></td>
+                            <td><?php echo $produk['id_kategori'] ?></td>
+                            <td>
+                                <a href="#" class="btn btn-primary">Detail</a>
+                                <a href="#" class="btn btn-warning">Ubah</a>
+                                <a href="#" class="btn btn-danger">Hapus</a>
+                            </td>
+                        </tr>
+                        <?php $nomor++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
-          </div>
         </div>
-</div>
+    </div>
+</div> 
