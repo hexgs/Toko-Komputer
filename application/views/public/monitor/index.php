472bc6e8b5bf8d7d3012ff90ef037b1e061dee92
yogaibnu <yogaibnudinataa@gmail.com>
2019-04-09 21:20:24 +0700

    perbaikan parameter index pada controller DetailProduk

--- application/views/public/monitor/index.php
+++ application/views/public/monitor/index.php
@@ -38,7 +38,7 @@
 												<h3 class="product-name"><a href="#"><?php echo $produk['nama_produk']?></a></h3>
 												<h4 class="product-price">Rp. <?php echo number_format($produk['harga'])?></h4>
 												<div class="product-btns">
-													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>produk';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>detailproduk';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
 												</div>
 											</div>
 											<div class="add-to-cart">
