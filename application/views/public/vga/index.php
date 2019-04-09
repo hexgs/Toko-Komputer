a298ac0221935b6bc9f2fa8f213911737992d3f2
PadangBasudewa <padangbasudewa4@gmail.com>
2019-04-09 21:20:14 +0700

    index VGA

--- application/views/public/vga/index.php
+++ application/views/public/vga/index.php
@@ -1,3 +1,4 @@
+<!-- <?php var_dump($vga);?> -->
 <!-- SECTION -->
 <div class="section">
 			<!-- container -->
@@ -8,7 +9,7 @@
 					<!-- section title -->
 					<div class="col-md-12">
 						<div class="section-title">
-							<h3 class="title">Processor</h3>
+							<h3 class="title">VGA</h3>
 						</div>
 					</div>
 					<!-- /section title -->
@@ -16,13 +17,15 @@
 
 					<!-- NEW PRODUCT (CONNECT DATABASE) -->
 					<!-- Products tab & slick -->
-					<div class="col-md-12">
-						<div class="row">
+					
+					<div class="col-md-12">					
+						<div class="row">                		
 							<div class="products-tabs">
 								<!-- tab -->
 								<div id="tab1" class="tab-pane active">
 									<div class="products-slick" data-nav="#slick-nav-1">
-										<!-- product -->
+									<?php foreach($vga as $produk): ?>
+										<!-- product -->										
 										<div class="product">
 											<div class="product-img">
 												<img src="<?= config_item('base_url()'); ?>assets/front/img/product01.png" alt="">
@@ -31,124 +34,26 @@
 												</div>
 											</div>
 											<div class="product-body">
-												<p class="product-category">Category</p>
-												<h3 class="product-name"><a href="#">product name goes here</a></h3>
-												<h4 class="product-price">$980.00 <!-- <del class="product-old-price">$990.00</del> --></h4>
-												<!-- <div class="product-rating">
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-													<i class="fa fa-star"></i>
-												</div> -->
+												<!-- <p class="product-category"><?php echo $produk['id_kategori']?></p> -->
+												<h3 class="product-name"><a href="#"><?php echo $produk['nama_produk']?></a></h3>
+												<h4 class="product-price">Rp. <?php echo number_format($produk['harga'])?></h4>
 												<div class="product-btns">
 													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>produk';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
 												</div>
 											</div>
 											<div class="add-to-cart">
 												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-											</div>
+											</div>											
 										</div>
-										<!-- /product -->
-
-										<!-- product -->
-										<div class="product">
-											<div class="product-img">
-												<img src="<?= config_item('base_url()'); ?>assets/front/img/product02.png" alt="">
-												<div class="product-label">
-													<span class="new">NEW</span>
-												</div>
-											</div>
-											<div class="product-body">
-												<p class="product-category">Category</p>
-												<h3 class="product-name"><a href="#">product name goes here</a></h3>
-												<h4 class="product-price">$980.00 </h4>
-												
-												<div class="product-btns">
-													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>produk'';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-											</div>
-										</div>
-										<!-- /product -->
-
-										<!-- product -->
-										<div class="product">
-											<div class="product-img">
-												<img src="<?= config_item('base_url()'); ?>assets/front/img/product03.png" alt="">
-												<div class="product-label">
-													<span class="new">NEW</span>
-												</div>
-											</div>
-											<div class="product-body">
-												<p class="product-category">Category</p>
-												<h3 class="product-name"><a href="#">product name goes here</a></h3>
-												<h4 class="product-price">$980.00 </h4>
-												
-												<div class="product-btns">
-													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>produk'';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-											</div>
-										</div>
-										<!-- /product -->
-
-										<!-- product -->
-										<div class="product">
-											<div class="product-img">
-												<img src="<?= config_item('base_url()'); ?>assets/front/img/product04.png" alt="">
-												<div class="product-label">
-													<span class="new">NEW</span>
-												</div>
-											</div>
-											<div class="product-body">
-												<p class="product-category">Category</p>
-												<h3 class="product-name"><a href="#">product name goes here</a></h3>
-												<h4 class="product-price">$980.00 </h4>
-												
-												<div class="product-btns">
-													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>produk'';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-											</div>
-										</div>
-										<!-- /product -->
-
-										<!-- product -->
-										<div class="product">
-											<div class="product-img">
-												<img src="<?= config_item('base_url()'); ?>assets/front/img/product05.png" alt="">
-												<div class="product-label">
-													<span class="new">NEW</span>
-												</div>
-											</div>
-											<div class="product-body">
-												<p class="product-category">Category</p>
-												<h3 class="product-name"><a href="#">product name goes here</a></h3>
-												<h4 class="product-price">$980.00 </h4>
-												
-												<div class="product-btns">
-													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>produk'';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-											</div>
-										</div>
-										<!-- /product -->
-									</div>
+										<!-- /product -->	
+										<?php endforeach; ?>									
+									</div>								
 									<div id="slick-nav-1" class="products-slick-nav"></div>
 								</div>
 								<!-- /tab -->
 							</div>
-						</div>
-					</div>
+						</div>						
+					</div>					
 					<!-- Products tab & slick -->
 				</div>
 				<!-- /row -->
