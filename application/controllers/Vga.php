a6351f18a7a70c442d0c0f1ae3f947856ea10dd1
PadangBasudewa <padangbasudewa4@gmail.com>
2019-04-09 21:15:37 +0700

    Mengganti Function class VGA

--- application/controllers/Vga.php
+++ application/controllers/Vga.php
@@ -3,14 +3,14 @@
 class Vga extends CI_Controller {
     public function __construct() {
         parent::__construct();
-
+        $this->load->model('Vga_model');
     }
 
     public function index($user = '') {
         $data['judul'] = 'Toko Komputer';
-        //$data['user'] = $user;
+        $data['vga'] = $this->Vga_model->getVga();
         $this->load->view('public/templates/header', $data);
-        $this->load->view('public/vga/index');
+        $this->load->view('public/vga/index', $data);
         $this->load->view('public/templates/footer');
     }
 }
