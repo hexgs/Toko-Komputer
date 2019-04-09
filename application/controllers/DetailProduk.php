eb87770f7cf0aac9ad41befd6f357230db153ab8
yogaibnu <yogaibnudinataa@gmail.com>
2019-04-09 21:11:28 +0700

    change fungsi index controller DetailProduk

--- application/controllers/DetailProduk.php
+++ application/controllers/DetailProduk.php
@@ -3,14 +3,14 @@
 class DetailProduk extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        
+        $this->load->model('DetailProduk_model');
     }
 
-    public function index($id = '') {
+    public function index($id_produk) {
         $data['judul'] = 'Toko Komputer';
-        //$data['user'] = $user;
+        $data['detail_produk'] = $this->DetailProduk_model->getDetailProduk($id_produk);
         $this->load->view('public/templates/header', $data);
-        $this->load->view('public/home/index');
+        $this->load->view('public/detailproduk/index', $data);
         $this->load->view('public/templates/footer');
     }
 }
