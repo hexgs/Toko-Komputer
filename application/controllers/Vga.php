00f65be3990bd194d89514c50944b0ddc693c75c
febritoriqkhul <febritoriqkhul@gmail.com>
2019-04-09 21:48:40 +0700

    Edit view item

--- application/controllers/Vga.php
+++ application/controllers/Vga.php
@@ -3,12 +3,12 @@
 class Vga extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->model('Vga_model');
+        $this->load->model('Item_model');
     }
 
     public function index($user = '') {
         $data['judul'] = 'Toko Komputer';
-        $data['vga'] = $this->Vga_model->getVga();
+        $data['vga'] = $this->Item_model->getVga();
         $this->load->view('public/templates/header', $data);
         $this->load->view('public/vga/index', $data);
         $this->load->view('public/templates/footer');
--- application/models/Vga_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Vga_model extends CI_model {
-    public function getVga() {
-        return $this->db->get_where('produk', ['id_kategori' => 1])->result_array();
-        // return $this->db->get('produk')->result_array();
-    }
-}
