75c45394cb35cb3fe01ceee8143337826384918f
irhamrizaldy <rirham12@gmail.com>
2019-04-10 16:48:33 +0700

    make function tampilData($id)

--- application/models/DetailProduk_model.php
+++ application/models/DetailProduk_model.php
@@ -1,9 +1,17 @@
 <?php
 
-class DetailProduk_model extends CI_model {
-    public function getDetailProduk($id) {
-        // return $this->db->get_where('produk', ['id_produk' => 3])->row();
-        return $this->db->where('id_produk',$id)->get('produk')->row();
-        // return $this->db->get('produk')->result_array();
+class DetailProduk_model extends CI_model
+{
+    // public function getDetailProduk($id) {
+    //     // return $this->db->get_where('produk', ['id_produk' => 3])->row();
+    //     return $this->db->where('id_produk',$id)->get('produk')->row();
+    //     // return $this->db->get('produk')->result_array();
+    // }
+
+    public function tampilData($id)
+    {
+        return $this->db->where('id_produk', $id)
+            ->get('produk')
+            ->row();
     }
 }
