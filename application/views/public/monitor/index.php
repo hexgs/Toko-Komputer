57d8e4f082ab8f8fa1f3a0d50cbb8d167a512c93
irhamrizaldy <rirham12@gmail.com>
2019-04-10 16:44:15 +0700

    Make function to get product from id

--- application/views/public/monitor/index.php
+++ application/views/public/monitor/index.php
@@ -1,65 +1,65 @@
-<!-- <?php var_dump($monitor);?> -->
+<!-- <?php var_dump($monitor); ?> -->
 <!-- SECTION -->
 <div class="section">
-			<!-- container -->
-			<div class="container">
-				<!-- row -->
-				<div class="row">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
 
-					<!-- section title -->
-					<div class="col-md-12">
-						<div class="section-title">
-							<h3 class="title">Monitor</h3>
-						</div>
-					</div>
-					<!-- /section title -->
-					
-
-					<!-- NEW PRODUCT (CONNECT DATABASE) -->
-					<!-- Products tab & slick -->
-					
-					<div class="col-md-12">					
-						<div class="row">                		
-							<div class="products-tabs">
-								<!-- tab -->
-								<div id="tab1" class="tab-pane active">
-									<div class="products-slick" data-nav="#slick-nav-1">
-									<?php foreach($monitor as $produk): ?>
-										<!-- product -->										
-										<div class="product">
-											<div class="product-img">
-												<img src="<?= config_item('base_url()'); ?>assets/front/img/product01.png" alt="">
-												<div class="product-label">
-													<span class="new">NEW</span>
-												</div>
-											</div>
-											<div class="product-body">
-												<!-- <p class="product-category"><?php echo $produk['id_kategori']?></p> -->
-												<h3 class="product-name"><a href="#"><?php echo $produk['nama_produk']?></a></h3>
-												<h4 class="product-price">Rp. <?php echo number_format($produk['harga'])?></h4>
-												<div class="product-btns">
-													<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>detailproduk';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-													<!-- <a href="<?php echo base_url('detailproduk').$produk->id_produk; ?>" class="btn btn-default">Detail</a> -->
-													
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-											</div>											
-										</div>
-										<!-- /product -->	
-										<?php endforeach; ?>									
-									</div>								
-									<div id="slick-nav-1" class="products-slick-nav"></div>
-								</div>
-								<!-- /tab -->
-							</div>
-						</div>						
-					</div>					
-					<!-- Products tab & slick -->
+			<!-- section title -->
+			<div class="col-md-12">
+				<div class="section-title">
+					<h3 class="title">Monitor</h3>
 				</div>
-				<!-- /row -->
 			</div>
-			<!-- /container -->
+			<!-- /section title -->
+
+
+			<!-- NEW PRODUCT (CONNECT DATABASE) -->
+			<!-- Products tab & slick -->
+
+			<div class="col-md-12">
+				<div class="row">
+					<div class="products-tabs">
+						<!-- tab -->
+						<div id="tab1" class="tab-pane active">
+							<div class="products-slick" data-nav="#slick-nav-1">
+								<?php foreach ($monitor as $produk) : ?>
+									<!-- product -->
+									<div class="product">
+										<div class="product-img">
+											<img src="<?= config_item('base_url()'); ?>assets/front/img/product01.png" alt="">
+											<div class="product-label">
+												<span class="new">NEW</span>
+											</div>
+										</div>
+										<div class="product-body">
+											<!-- <p class="product-category"><?php echo $produk['id_kategori'] ?></p> -->
+											<h3 class="product-name"><a href="#"><?php echo $produk['nama_produk'] ?></a></h3>
+											<h4 class="product-price">Rp. <?php echo number_format($produk['harga']) ?></h4>
+											<div class="product-btns">
+												<button class="quick-view" onclick="window.location.href = '<?= base_url(); ?>detailproduk/detail/<?= $produk['id_produk']; ?>';"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+												<!-- <a href="<?php echo base_url('detailproduk') . $produk->id_produk; ?>" class="btn btn-default">Detail</a> -->
+
+											</div>
+										</div>
+										<div class="add-to-cart">
+											<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+										</div>
+									</div>
+									<!-- /product -->
+								<?php endforeach; ?>
+							</div>
+							<div id="slick-nav-1" class="products-slick-nav"></div>
+						</div>
+						<!-- /tab -->
+					</div>
+				</div>
+			</div>
+			<!-- Products tab & slick -->
 		</div>
-		<!-- /SECTION -->
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</div>
+<!-- /SECTION -->
