19c5bbece381795cd133f8a618019c99ced30875
irhamrizaldy <rirham12@gmail.com>
2019-04-11 01:07:42 +0700

    bootstrap

--- application/views/public/cart/index.php
+++ application/views/public/cart/index.php
@@ -1,4 +1,4 @@
-<!-- <?php var_dump($cart)?>
+<!-- <?php var_dump($cart) ?>
 	<div class="container">
 		<h1>Cart</h1>
 		<hr>
@@ -14,7 +14,7 @@
 				</tr>
 			</thead>
 			<tbody>
-			<?php $nomor = 1;?>
+			<?php $nomor = 1; ?>
 			
 				<tr>
 					<td><?php echo $nomor; ?></td>
@@ -34,61 +34,62 @@
 		<a href="<?= base_url(); ?>home" class="btn btn-default">Lanjutkan Belanja</a>
 		<a href="<?= base_url(); ?>checkout" class="btn btn-primary">Checkout</a>
 	</div> -->
+<div class="container">
+	<h1>Cart</h1>
+	<?php echo form_open('cart/update'); ?>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Nama Barang</th>
+				<th>Jumlah</th>
+				<th>Harga</th>
+				<th>Sub-Total</th>
+				<th>Aksi</th>
+			</tr>
+		</thead>
+		<?php $i = 1; ?>
 
-<?php echo form_open('cart/update'); ?>
-<table cellpadding="6" cellspacing="1" border="1">
+		<?php foreach ($this->cart->contents() as $items) : ?>
 
-<tr>
-		<th>No</th>
-        <th>Nama Barang</th>
-        <th>Jumlah</th>
-        <th>Harga</th>
-        <th>Sub-Total</th>
-		<th>Aksi</th>
-</tr>
+			<?php echo form_hidden($i . '[rowid]', $items['rowid']); ?>
 
-<?php $i = 1; ?>
-
-<?php foreach ($this->cart->contents() as $items): ?>
-
-        <?php echo form_hidden($i.'[rowid]', $items['rowid']); ?>
-
-        <tr>  
-                <td><?php echo $i; ?></td>
+			<tr>
+				<td><?php echo $i; ?></td>
 				<td>
-                        <?php echo $items['name']; ?>
+					<?php echo $items['name']; ?>
 
-                        <?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
+					<?php if ($this->cart->has_options($items['rowid']) == TRUE) : ?>
 
-                                <p>
-                                        <?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
+						<p>
+							<?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value) : ?>
 
-                                                <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
+								<strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
 
-                                        <?php endforeach; ?>
-                                </p>
+							<?php endforeach; ?>
+						</p>
 
-                        <?php endif; ?>
+					<?php endif; ?>
 
-                </td>
-				<td><?php echo form_input(array('name' => 'qty'.$i, 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
-                <td>Rp. <?php echo $this->cart->format_number($items['price']); ?></td>
-                <td>Rp. <?php echo $this->cart->format_number($items['subtotal']); ?></td>
+				</td>
+				<td><?php echo form_input(array('name' => 'qty' . $i, 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
+				<td>Rp. <?php echo $this->cart->format_number($items['price']); ?></td>
+				<td>Rp. <?php echo $this->cart->format_number($items['subtotal']); ?></td>
 				<td><a href="<?= base_url(); ?>cart/delete/<?= $items['rowid']; ?>" class="btn btn-danger btn-xs">Hapus</a></td>
-        </tr>
+			</tr>
 
-<?php $i++; ?>
+			<?php $i++; ?>
 
-<?php endforeach; ?>
+		<?php endforeach; ?>
 
-<tr>
-        <td colspan="3"> </td>
-        <td class="right"><strong>Total</strong></td>
-        <td class="right">Rp. <?php echo $this->cart->format_number($this->cart->total()); ?></td>
-</tr>
+		<tr>
+			<td colspan="3"> </td>
+			<td class="right"><strong>Total</strong></td>
+			<td class="right">Rp. <?php echo $this->cart->format_number($this->cart->total()); ?></td>
+		</tr>
 
-</table>
-<br>
-<a href="<?= base_url(); ?>home" class="btn btn-default">Lanjutkan Belanja</a>
-<?php echo form_submit('', 'Update your Cart'); ?>
-<?php form_close(); ?>
+	</table>
+	<a href="<?= base_url(); ?>home" class="btn btn-default">Lanjutkan Belanja</a>
+	<?php echo form_submit('', 'Update your Cart', "class='btn btn-primary'"); ?>
+	<?php form_close(); ?>
+</div>
