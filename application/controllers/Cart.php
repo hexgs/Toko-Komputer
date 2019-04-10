543e948d026ca819d181b7f99dbd9e0b73c763b8
yogaibnu <yogaibnudinataa@gmail.com>
2019-04-10 22:57:47 +0700

    perbaikan cart

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -15,15 +15,53 @@ class Cart extends CI_Controller
         // $this->load->view('public/templates/header', $data);
         // $this->load->view('public/detailproduk/index', $data);
         // $this->load->view('public/templates/footer');
+        
         $data['judul'] = 'Toko Komputer';
+        
         $this->load->view('public/templates/dashboard', $data);
     }
 
+    // public function addCart($id_produk)
+    // {
+    //     $data['judul'] = 'Toko Komputer';
+    //     $data['content'] = 'public/cart/index';
+    //     $data['cart'] = $this->Item_model->tampilCart($id_produk);
+    //     $this->load->view('public/templates/dashboard', $data);
+    // }
+
     public function addCart($id_produk)
     {
-        $data['judul'] = 'Toko Komputer';
-        $data['content'] = 'public/cart/index';
-        $data['cart'] = $this->Item_model->tampilCart($id_produk);
-        $this->load->view('public/templates/dashboard', $data);
+        // $data['judul'] = 'Toko Komputer';
+        // $data['content'] = 'public/cart/index';
+
+        $produk = $this->Item_model->tampilCart($id_produk);
+        $data = array(
+            'id'      => $produk->id_produk,
+            'qty'     => 1,
+            'price'   => $produk->harga,
+            'name'    => $produk->nama_produk
+            );
+        $this->cart->insert($data);
+
+        $this->load->view('public/cart/index', $data);
+
+        // $this->load->view('public/templates/dashboard', $data);
+    }
+
+    public function delete($rowid)
+    {
+        $this->cart->update(array('rowid' => $rowid, 'qty' => 0));
+        $this->load->view('public/cart/index'); 
+    }
+
+    public function update()
+    {
+        $i = 1;
+        foreach ($this->cart->contents() as $items)
+        {
+            $this->cart->update(array('rowid' => $items['rowid'], 'qty' => $_POST['qty'.$i]));
+            $i++;
+        }
+        $this->load->view('public/cart/index'); 
     }
 }
--- application/views/public/cart/index.php
+++ application/views/public/cart/index.php
@@ -1,4 +1,4 @@
-<?php var_dump($produk)?>
+<!-- <?php var_dump($cart)?>
 	<div class="container">
 		<h1>Cart</h1>
 		<hr>
@@ -14,9 +14,11 @@
 				</tr>
 			</thead>
 			<tbody>
+			<?php $nomor = 1;?>
+			
 				<tr>
-					<td><?php echo '1'; ?></td>
-					<td><?php echo 'monitor'; ?></td>
+					<td><?php echo $nomor; ?></td>
+					<td><?= $cart->nama_produk; ?></td>
 					<td><?php echo '$988.00'; ?></td>
 					<td><?php echo '1'; ?></td>
 					<td><?php echo '$988.00'; ?></td>
@@ -24,11 +26,69 @@
 						<a href="" class="btn btn-danger btn-xs">Hapus</a>
 					</td>
 				</tr>
-
+				
 			
 			</tbody>
 		</table>
 
 		<a href="<?= base_url(); ?>home" class="btn btn-default">Lanjutkan Belanja</a>
 		<a href="<?= base_url(); ?>checkout" class="btn btn-primary">Checkout</a>
-	</div>
+	</div> -->
+
+<?php echo form_open('cart/update'); ?>
+<table cellpadding="6" cellspacing="1" border="1">
+
+<tr>
+		<th>No</th>
+        <th>Nama Barang</th>
+        <th>Jumlah</th>
+        <th>Harga</th>
+        <th>Sub-Total</th>
+		<th>Aksi</th>
+</tr>
+
+<?php $i = 1; ?>
+
+<?php foreach ($this->cart->contents() as $items): ?>
+
+        <?php echo form_hidden($i.'[rowid]', $items['rowid']); ?>
+
+        <tr>  
+                <td><?php echo $i; ?></td>
+				<td>
+                        <?php echo $items['name']; ?>
+
+                        <?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
+
+                                <p>
+                                        <?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
+
+                                                <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
+
+                                        <?php endforeach; ?>
+                                </p>
+
+                        <?php endif; ?>
+
+                </td>
+				<td><?php echo form_input(array('name' => 'qty'.$i, 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
+                <td>Rp. <?php echo $this->cart->format_number($items['price']); ?></td>
+                <td>Rp. <?php echo $this->cart->format_number($items['subtotal']); ?></td>
+				<td><a href="<?= base_url(); ?>cart/delete/<?= $items['rowid']; ?>" class="btn btn-danger btn-xs">Hapus</a></td>
+        </tr>
+
+<?php $i++; ?>
+
+<?php endforeach; ?>
+
+<tr>
+        <td colspan="3"> </td>
+        <td class="right"><strong>Total</strong></td>
+        <td class="right">Rp. <?php echo $this->cart->format_number($this->cart->total()); ?></td>
+</tr>
+
+</table>
+<br>
+<a href="<?= base_url(); ?>home" class="btn btn-default">Lanjutkan Belanja</a>
+<?php echo form_submit('', 'Update your Cart'); ?>
+<?php form_close(); ?>
