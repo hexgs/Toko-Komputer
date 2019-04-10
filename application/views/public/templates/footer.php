2984f3b23d98a0f5532d0b160913714fc6eb8e52
irhamrizaldy <rirham12@gmail.com>
2019-04-10 16:42:32 +0700

    Change location base_url

--- application/views/public/templates/footer.php
+++ application/views/public/templates/footer.php
@@ -1,65 +1,66 @@
 				<br>
 				<!-- FOOTER -->
-                <footer id="footer">
-			<!-- top footer -->
-			<div class="section">
-				<!-- container -->
-				<div class="container">
-					<!-- row -->
-					<div class="row">
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">About Us</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
-								<ul class="footer-links">
-									<li><a href="#"><i class="fa fa-map-marker"></i>Jalan Sawojajar</a></li>
-									<li><a href="#"><i class="fa fa-phone"></i>+021-95-51-84</a></li>
-									<li><a href="#"><i class="fa fa-envelope-o"></i>tokokomputer@gmail.com</a></li>
-								</ul>
+				<footer id="footer">
+					<!-- top footer -->
+					<div class="section">
+						<!-- container -->
+						<div class="container">
+							<!-- row -->
+							<div class="row">
+								<div class="col-md-3 col-xs-6">
+									<div class="footer">
+										<h3 class="footer-title">About Us</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+										<ul class="footer-links">
+											<li><a href="#"><i class="fa fa-map-marker"></i>Jalan Sawojajar</a></li>
+											<li><a href="#"><i class="fa fa-phone"></i>+021-95-51-84</a></li>
+											<li><a href="#"><i class="fa fa-envelope-o"></i>tokokomputer@gmail.com</a></li>
+										</ul>
+									</div>
+								</div>
+
+								<div class="col-md-3 col-xs-6">
+									<div class="footer">
+										<h3 class="footer-title">Categories</h3>
+										<ul class="footer-links">
+											<li><a href="<?= base_url(); ?>monitor">Monitor</a></li>
+											<li><a href="<?= base_url(); ?>laptops">Laptops</a></li>
+											<li><a href="<?= base_url(); ?>vga">VGA</a></li>
+											<li><a href="<?= base_url(); ?>mouse">Mouse</a></li>
+											<li><a href="<?= base_url(); ?>keyboard">Keyboard</a></li>
+										</ul>
+									</div>
+								</div>
+
+								<div class="clearfix visible-xs"></div>
+
+								<div class="col-md-3 col-xs-6">
+									<div class="footer">
+										<h3 class="footer-title">Keamanan</h3>
+										<!-- di isi -->
+										<p>aman bosku</p>
+									</div>
+								</div>
+
+
 							</div>
+							<!-- /row -->
 						</div>
-
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">Categories</h3>
-								<ul class="footer-links">
-								<li><a href="<?= base_url(); ?>monitor">Monitor</a></li>
-								<li><a href="<?= base_url(); ?>laptops">Laptops</a></li>
-								<li><a href="<?= base_url(); ?>vga">VGA</a></li>
-								<li><a href="<?= base_url(); ?>mouse">Mouse</a></li>
-								<li><a href="<?= base_url(); ?>keyboard">Keyboard</a></li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="clearfix visible-xs"></div>
-
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">Keamanan</h3>
-								<!-- di isi -->
-								<p>aman bosku</p>
-							</div>
-						</div>
-
-						
+						<!-- /container -->
 					</div>
-					<!-- /row -->
-				</div>
-				<!-- /container -->
-			</div>
-			<!-- /top footer -->
+					<!-- /top footer -->
 
-		</footer>
-		<!-- /FOOTER -->
+				</footer>
+				<!-- /FOOTER -->
 
-		<!-- jQuery Plugins -->
-		<script src="<?= config_item('base_url()'); ?>assets/front/js/jquery.min.js"></script>
-		<script src="<?= config_item('base_url()'); ?>assets/front/js/bootstrap.min.js"></script>
-		<script src="<?= config_item('base_url()'); ?>assets/front/js/slick.min.js"></script>
-		<script src="<?= config_item('base_url()'); ?>assets/front/js/nouislider.min.js"></script>
-		<script src="<?= config_item('base_url()'); ?>assets/front/js/jquery.zoom.min.js"></script>
-		<script src="<?= config_item('base_url()'); ?>assets/front/js/main.js"></script>
+				<!-- jQuery Plugins -->
+				<script src="<?= base_url(); ?>assets/front/js/jquery.min.js"></script>
+				<script src="<?= base_url(); ?>assets/front/js/bootstrap.min.js"></script>
+				<script src="<?= base_url(); ?>assets/front/js/slick.min.js"></script>
+				<script src="<?= base_url(); ?>assets/front/js/nouislider.min.js"></script>
+				<script src="<?= base_url(); ?>assets/front/js/jquery.zoom.min.js"></script>
+				<script src="<?= base_url(); ?>assets/front/js/main.js"></script>
 
-	</body>
-</html>
+				</body>
+
+				</html>
