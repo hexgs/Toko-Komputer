1cbea4d3bef5dd8872c9901da613a2f8b9c445a3
irhamrizaldy <rirham12@gmail.com>
2019-04-10 16:43:11 +0700

    Little changes

--- application/views/public/templates/dashboard.php
+++ application/views/public/templates/dashboard.php
@@ -1,45 +1,24 @@
-
-<!DOCTYPE html>
 <html>
+<?php $this->load->view('public/templates/header'); ?>
 
-<?php $this->load->view('admin/templates/header'); ?>
+<body>
+  <div class="wrapper">
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Main content -->
+      <section class="content">
+        <!-- Small boxes (Stat box) -->
+        <?php $this->load->view($content); ?>
+        <!-- /.row -->
+        <!-- Main row -->
 
-<body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+        <!-- /.row (main row) -->
 
-
-
-  <!-- Left side column. contains the logo and sidebar -->
-  
-
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        <?php echo $judul ?>
-        <!-- <small>Control panel</small> -->
-      </h1>
-      
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <!-- Small boxes (Stat box) -->
-      <?php $this->load->view($content); ?>
-      <!-- /.row -->
-      <!-- Main row -->
-      
-      <!-- /.row (main row) -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  
-  <?php $this->load->view('public/templates/footer');  ?>
-
-
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <?php $this->load->view('public/templates/footer');  ?>
 </body>
+
 </html>
