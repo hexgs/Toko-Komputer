33febc5f243cd8772aca4c905fd1dc832d743b8c
irhamrizaldy <rirham12@gmail.com>
2019-04-10 16:49:15 +0700

    make function detail to get product based on id

--- application/controllers/DetailProduk.php
+++ application/controllers/DetailProduk.php
@@ -1,16 +1,29 @@
 <?php
 
-class DetailProduk extends CI_Controller {
-    public function __construct() {
+class DetailProduk extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('DetailProduk_model');
     }
 
-    public function index($id_produk=3) {
+    public function index()
+    {
+        // $data['judul'] = 'Toko Komputer';
+        // $data['detail_produk'] = $this->DetailProduk_model->getDetailProduk($id_produk);
+        // $this->load->view('public/templates/header', $data);
+        // $this->load->view('public/detailproduk/index', $data);
+        // $this->load->view('public/templates/footer');
         $data['judul'] = 'Toko Komputer';
-        $data['detail_produk'] = $this->DetailProduk_model->getDetailProduk($id_produk);
-        $this->load->view('public/templates/header', $data);
-        $this->load->view('public/detailproduk/index', $data);
-        $this->load->view('public/templates/footer');
+        $this->load->view('public/templates/dashboard', $data);
+    }
+
+    public function detail($id_produk)
+    {
+        $data['judul'] = 'Toko Komputer';
+        $data['content'] = 'public/detailproduk/index';
+        $data['detail_produk'] = $this->DetailProduk_model->tampilData($id_produk);
+        $this->load->view('public/templates/dashboard', $data);
     }
 }
