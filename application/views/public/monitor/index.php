e1a1146bec81bea6bed2fedea50dcfd92db4f1e4
irhamrizaldy <rirham12@gmail.com>
2019-04-11 21:14:16 +0700

    monitor/index

--- application/views/public/monitor/index.php
+++ application/views/public/monitor/index.php
@@ -39,7 +39,7 @@
 											</div>
 										</div>
 										<div class="add-to-cart">
-										<button class="add-to-cart-btn" onclick="window.location.href = '<?= base_url(); ?>cart/addCart/<?= $produk['id_produk']; ?>';"><i class="fa fa-shopping-cart"></i> add to cart</button>
+											<button class="add-to-cart-btn" onclick="window.location.href = '<?= base_url(); ?>cart/addCart/<?= $produk['id_produk']; ?>';"><i class="fa fa-shopping-cart"></i> add to cart</button>
 										</div>
 									</div>
 									<!-- /product -->
