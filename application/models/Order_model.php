3b2881fe938386af33360b3da549930c3566a032
yogaibnu <yogaibnudinataa@gmail.com>
2019-04-11 22:33:29 +0700

    membuat model OrderDetail

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Order_model extends CI_Model 
 {
     public function create ($order) {
-        
+        $this->db->insert('pembelian', $order);
+        return $this->db->insert_id();
     }
 }
