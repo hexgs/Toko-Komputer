3f3cfcf4954c0c31905adefc4e21e3a4819da320
irhamrizaldy <rirham12@gmail.com>
2019-04-11 21:14:36 +0700

    public/login/index

--- application/views/public/login/index.php
+++ application/views/public/login/index.php
@@ -26,37 +26,37 @@
   </div>
 </form> -->
 <div class="container">
-<div class="row content-center">
-  <div class="col-md-8">
-		<div class="imgcontainer">
-			<img style="width:150px;height:150px;" src="<?= config_item('base_url()'); ?>assets/FRONT/img/default.png" alt="">
-		</div>
-    <!-- <h2>Login Form</h2> -->
-    <?php if(validation_errors()) { ?>
-      <div class="alert alert-danger">
-        <?php echo validation_errors(); ?>
+  <div class="row content-center">
+    <div class="col-md-8">
+      <div class="imgcontainer">
+        <img style="width:150px;height:150px;" src="<?= base_url(); ?>assets/FRONT/img/default.png" alt="">
       </div>
-    <?php } ?>
-    <?php if(!empty($this->input->get('msg')) && $this->input->get('msg') == 1) { ?>
-      <div class="alert alert-danger">
-        Please Enter Your Valid Information.
-      </div>
-    <?php } ?>
-    <?php echo form_open('login/dologin'); ?>
-          
+      <!-- <h2>Login Form</h2> -->
+      <?php if (validation_errors()) { ?>
+        <div class="alert alert-danger">
+          <?php echo validation_errors(); ?>
+        </div>
+      <?php } ?>
+      <?php if ($this->input->get('msg') == 1) { ?>
+        <div class="alert alert-danger">
+          Please Enter Your Valid Information.
+        </div>
+      <?php } ?>
+      <?php echo form_open('login/dologin'); ?>
+
       <div class="form-group">
-         <input type="text" name="email_pelanggan" class="form-control" id="email_pelanggan" placeholder="Email" required>
+        <input type="text" name="email_pelanggan" class="form-control" id="email_pelanggan" placeholder="Email" required>
       </div>
       <div class="form-group">
-         <input type="password" name="password_pelanggan" class="form-control" id="password_pelanggan" placeholder="Password" required>
-      </div>  
+        <input type="password" name="password_pelanggan" class="form-control" id="password_pelanggan" placeholder="Password" required>
+      </div>
       <div class="form-group pull-right">
-       <button type="submit" id="login" class="btn btn-primary">Login</button>
+        <button type="submit" id="login" class="btn btn-primary">Login</button>
+      </div>
+
     </div>
-      
-    </div>
-    
+
     <?php echo form_close(); ?>
-  
-</div>
+
+  </div>
 </div>
