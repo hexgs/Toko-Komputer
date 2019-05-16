b25c13626699d3ecaab097e8277ecd59cf3bcb48
irhamrizaldy <rirham12@gmail.com>
2019-05-17 02:10:02 +0700

    views detailTransaksi

--- application/views/admin/detailTransaksi.php
+++ application/views/admin/detailTransaksi.php
@@ -3,44 +3,37 @@
         <div class="box">
             <div class="box-header">
                 <h3 class="box-title">Detail Transaksi</h3>
-                <hr>
-                <div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
-                <?php if ($this->session->flashdata('flash')) : ?>
-                <?php endif; ?>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-                <?php foreach ($transact as $tsc) : ?>
+                <?php foreach ($pembelian as $pb) : ?>
                     <div class="col-md-6 col-sm-6">
                         <table class="table table-striped">
                             <tr>
                                 <td>Nama Pelanggan</td>
-                                <td>: <?= $tsc['nama_pelanggan'] ?></td>
+                                <td>: <?= $pb['nama_pelanggan'] ?></td>
                             </tr>
                             <tr>
                                 <td>Tanggal Pembelian</td>
-                                <td>: <?= $tsc['tanggal_pembelian'] ?></td>
+                                <td>: <?= $pb['tanggal_pembelian'] ?></td>
                             </tr>
                             <tr>
                                 <td>Total Pembelian</td>
-                                <td>: <?= $tsc['total_pembelian'] ?></td>
+                                <td>: <?= $pb['total_pembelian'] ?></td>
                             </tr>
                             <tr>
                                 <td>Alamat</td>
-                                <td>: <?= $tsc['alamat'] ?></td>
+                                <td>: <?= $pb['alamat'] ?></td>
                             </tr>
                             <tr>
                                 <td>Bukti Pembayaran</td>
-                                <td>: <?= $tsc['bukti_pembayaran'] ?></td>
-                            </tr>
-                            <tr>
-                                <td></td>
+                                <td>: <?= $pb['bukti_pembayaran'] ?></td>
                             </tr>
                         </table>
-                    <?php endforeach; ?>
-                    <a href="#" class="btn btn-default" onclick="window.history.go(-1)">Kembali</a>
-                    <a href="<?= base_url('') ?>admin/item/updateItem/<?= $tsc['id_produk'] ?>" class="btn btn-warning">Edit</a>
-                </div>
+                        <a href="#" class="btn btn-default" onclick="window.history.go(-1)">Kembali</a>
+                        <!-- <a href="<?= base_url('') ?>admin/item/updateItem/<?= $pb['id_pembelian'] ?>" class="btn btn-warning">Edit</a> -->
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
