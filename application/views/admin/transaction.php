2e3550f2496439dbe72b4a9b29ad16324ed3da5f
irhamrizaldy <rirham12@gmail.com>
2019-05-16 22:32:20 +0700

    link detailTransaksi page

--- application/views/admin/transaction.php
+++ application/views/admin/transaction.php
@@ -36,7 +36,7 @@
 								<td><?php echo $tsc['bukti_pembayaran'] ?></td>
 
 								<td>
-									<a href="#" class="btn btn-primary">Detail</a>
+									<a href="<?php echo base_url(); ?>admin/transaction/detailtransaksi" class="btn btn-primary">Detail</a>
 									<a href="#" class="btn btn-warning">Upload Resi</a>
 									<!-- <a href="<?= base_url(); ?>admin/transaction/deleteitem/<?= $tsc['id_pembelian'] ?>" class="btn btn-danger tombol-hapus">Hapus</a> -->
 								</td>
