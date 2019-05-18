5d48edfccb0aa0cb1cb1354d7e18788c4521d882
irhamrizaldy <rirham12@gmail.com>
2019-05-18 13:04:57 +0700

    views/admin/ubahitem

--- application/views/admin/ubahitem.php
+++ application/views/admin/ubahitem.php
@@ -2,25 +2,37 @@
 	<div class="col-xs-12">
 		<div class="box">
 			<div class="box-header">
-				<h3 class="box-title">Add Item</h3>
+				<h3 class="box-title">Ubah Item</h3>
 				<hr>
 			</div>
 			<!-- /.box-header -->
 			<div class="box-body">
 
-				<form class="form-horizontal form-label-left" action="<?= base_url('') ?>admin/item/additem" enctype="multipart/form-data" method="post">
+				<form class="form-horizontal form-label-left" action="<?= base_url('') ?>admin/item/updateitem" enctype="multipart/form-data" method="post">
 
 					<!-- <div class="form-group">
 						<label class="col-md-7 col-sm-6 col-xs-12">
 							<?php if (validation_errors()) : ?>
-									<div class="alert alert-danger" role="alert">
-										<?= validation_errors(); ?>
-									</div>
+								<div class="alert alert-danger" role="alert"><?= validation_errors(); ?></div>
 							<?php endif; ?>
 						</label>
 
 					</div> -->
 					<?php foreach ($item as $item) : ?>
+						<div class="form-group">
+							<label class="control-label col-md-2 col-sm-2 col-xs-12">Kategori</label>
+							<div class="col-md-4 col-sm-6">
+								<select name="id_kategori" class="form-control">
+									<option value="">--Pilih Kategori--</option>
+									<?php foreach ($category as $kategori) : ?>
+										<option value="<?= $kategori['id_kategori'] ?>"><?= $kategori['nama_kategori'] ?>
+										</option>
+									<?php endforeach; ?>
+								</select>
+								<small class="form-text text-danger"><?= form_error('id_kategori'); ?></small>
+							</div>
+						</div>
+
 						<div class="form-group">
 							<label class="control-label col-md-2 col-sm-2 col-xs-12">Nama Item
 							</label>
@@ -29,7 +41,6 @@
 								<small class="form-text text-danger"><?= form_error('nama_produk'); ?></small>
 							</div>
 						</div>
-
 
 						<div class="form-group">
 							<label class="control-label col-md-2 col-sm-2 col-xs-12">Harga Item
@@ -57,33 +68,6 @@
 								<p class="help-text">* Berat dalam satuan Gram</p>
 							</div>
 						</div>
-
-						<div class="form-group">
-							<label class="control-label col-md-2 col-sm-2 col-xs-12">Kategori</label>
-							<div class="col-md-4 col-sm-6">
-								<select name="id_kategori" class="form-control">
-									<option value="">--Pilih Kategori--</option>
-									<?php foreach ($category as $kategori) : ?>
-										<option value="<?= $kategori['id_kategori'] ?>"><?= $kategori['nama_kategori'] ?>
-										</option>
-									<?php endforeach; ?>
-								</select>
-								<small class="form-text text-danger"><?= form_error('id_kategori'); ?></small>
-							</div>
-						</div>
-
-						<!-- <div class="form-group">
-								<label class="control-label col-md-2 col-sm-2 col-xs-12">Status</label>
-								<div class="col-md-4 col-sm-6">
-									<select name="status" class="form-control">
-										<option value="">--Pilih Status--</option>
-										<option value="Active">Active</option>
-										<option value="Non Active">Non Active</option>
-									</select>
-									<small class="form-text text-danger"><?= form_error('status'); ?></small>
-								</div>
-							</div> -->
-
 						<div class="form-group">
 							<label class="control-label col-md-2 col-sm-2 col-xs-12">Gambar
 							</label>
@@ -97,9 +81,7 @@
 						<div class="form-group">
 							<label class="control-label col-md-2 col-sm-2 col-xs-12">Deskripsi</label>
 							<div class="col-md-9 col-sm-6">
-								<textarea class="form-control" rows="4" name="deskripsi">
-										<?= $item['deskripsi']; ?>
-									</textarea>
+								<textarea class="form-control" rows="4" name="deskripsi"><?= $item['deskripsi']; ?></textarea>
 								<small class="form-text text-danger"><?= form_error('deskripsi'); ?></small>
 							</div>
 						</div>
@@ -108,6 +90,7 @@
 
 					<div class="form-group">
 						<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+							<!-- <a href="<?= base_url('') ?>/admin/item/updateitem/<?= $item['id_produk']; ?>" class="btn btn-success">Submit</a> -->
 							<button type="submit" class="btn btn-success" name="submit" value="Submit">Submit</button>
 							<a href="<?= base_url('') ?>/admin/item" class="btn btn-primary">Kembali</a>
 						</div>
