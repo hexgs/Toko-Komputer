c0f06fc263ec29943069643400c2cd02b2cb2e9b
yogaibnu <yogaibnudinataa@gmail.com>
2019-05-23 22:41:32 +0700

    change bukti

--- application/views/public/cart/bukti.php
+++ application/views/public/cart/bukti.php
@@ -14,11 +14,21 @@
 					</div>
 					<p>Silahkan lakukan pembayaran ke rekening xxxxxx atas nama ZenAiKomputer.</p>
 					<p>Masukkan bukti pembayaran:</p>
-					<div class="form-group">
-						<textarea name="alamat" placeholder="alamat" class="form-control"></textarea>
-					</div>
-					
 
+					<div class="form-group">
+						<input type="text" name="nama_pelanggan" class="form-control" id="nama_pelanggan" placeholder="Nama" required>
+					</div>
+					<div class="form-group">
+						<input type="text" name="no_rekening" class="form-control" id="no_rekening" placeholder="No Rekening" required>
+					</div>
+					<div class="form-group">
+						<input type="text" name="jenis_bank" class="form-control" id="jenis_bank" placeholder="Jenis Bank" required>
+					</div>
+					<div class="form-group">
+						<input type="text" name="total_transfer" class="form-control" id="total_transfer" placeholder="Total Transfer" required>
+					</div>
+
+					<button class="btn btn-danger" type="submit" value="upload">Confirm</button>
 					<!-- <div class="form-group"> -->
 
 					<!-- <div class="col-md-5 col-sm-12 col-xs-12"> -->
@@ -28,7 +38,7 @@
 					<!-- <small class="form-text text-danger"><?= form_error('foto'); ?></small> -->
 					<!-- <p class="help-text">* Ukuran Gambar Maximal 2 Mb.</p> -->
 					<!-- <input type="submit" value="upload" /> -->
-					<button class="btn btn-danger" type="submit" value="upload">Upload</button>
+					<!-- <button class="btn btn-danger" type="submit" value="upload">Upload</button> -->
 					<!-- </div> -->
 					<!-- </div> -->
 					<!-- <a href="<?= base_url(); ?>cart/terimakasih" class="btn btn-danger">Submit</a> -->
